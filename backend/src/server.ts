8c257d3d5e51b323f9dc8d8bff663c11d2b88de9
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-07-21 17:39:50 +0200

    Remove unused csrf route and imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,61 +1,40 @@
-import dotenv from 'dotenv'; // Import dotenv for environment variables
-import express from 'express'; // Import express
-import mongoose from 'mongoose'; // Import mongoose
+import dotenv from "dotenv"; // Import dotenv for environment variables
+import express from "express"; // Import express
+import mongoose from "mongoose"; // Import mongoose
 // **************SECURITY
-import cors from 'cors'; // Import cors
-import helmet from 'helmet';
+import cors from "cors"; // Import cors
+import helmet from "helmet";
 // For at læse certifikatfilerne
-import fs from 'fs'; 
+import fs from "fs";
 // For at oprette en HTTPS-server
-import https from 'https'; 
-import { Request, Response, NextFunction } from 'express';
-import path from "path"; 
+import https from "https";
+import { Request, Response, NextFunction } from "express";
+import path from "path";
 import rateLimit from "express-rate-limit"; // Import rate limiting middleware
 
-
-
-
-
-
-
-
 // Routes
-import productsRouter from './routes/products'; // Use ES6 imports for routes
-import authRouter from './routes/auth'; 
+import productsRouter from "./routes/products"; // Use ES6 imports for routes
+import authRouter from "./routes/auth";
 import forgotPasswordRoutes from "./routes/user/forgotpassword";
-import authenticateJWT from './routes/authMiddleware';  // Importing the middleware
-import cartRoutes from './routes/cart';
-import ordersRoutes from "./routes/checkout/orders"; 
+import authenticateJWT from "./routes/authMiddleware"; // Importing the middleware
+import cartRoutes from "./routes/cart";
+import ordersRoutes from "./routes/checkout/orders";
 import deliveryRoutes from "./routes/checkout/delivery";
-import paymentRoute from './routes/checkout/payment';
+import paymentRoute from "./routes/checkout/payment";
 import favouritesRouter from "./routes/user/favorites";
 import adminProducts from "./routes/admin/adminProducts";
 import adminOrders from "./routes/admin/adminOrders";
 import userRouter from "./routes/user/userinfo";
-import recycleRoutes from './routes/recycle';  
-import csrfRoutes from "./routes/csrf";
-
-
+import recycleRoutes from "./routes/recycle";
 
 dotenv.config(); // Load environment variables from .env file
 
-
 const app = express();
 
-
-
-
-
-
-
 // **********************************//SECURITY MIDDLEWARES//******************************************
 // **********************************//SECURITY MIDDLEWARES//******************************************
 // **********************************//SECURITY MIDDLEWARES//******************************************
 // **********************************//SECURITY MIDDLEWARES//******************************************
-
-
-
-
 
 //************************************************************************ */ Helmet for security headers
 
@@ -66,38 +45,43 @@ const app = express();
 // Prevents XSS and data injection --> No injected JS into my site for example. Only trusted scripts can run.
 app.use(
   helmet({
-    frameguard: { action: 'sameorigin' }, // Prevents clickjacking
-    hidePoweredBy: true,                 // Hides "X-Powered-By: Express"
-    hsts: { maxAge: 31536000 },          // Enforce HTTPS for 1 year
-    noSniff: true,                       // Prevents MIME type sniffing
-    contentSecurityPolicy: {             // Your current CSP config
+    frameguard: { action: "sameorigin" }, // Prevents clickjacking
+    hidePoweredBy: true, // Hides "X-Powered-By: Express"
+    hsts: { maxAge: 31536000 }, // Enforce HTTPS for 1 year
+    noSniff: true, // Prevents MIME type sniffing
+    contentSecurityPolicy: {
+      // Your current CSP config
       directives: {
         defaultSrc: ["'self'"],
         // nonce-baseret CSP: Generer en ny tilfældig nonce ved hver request og tilføjer den i inline script
-        scriptSrc: ["'self'", "https://js.stripe.com", "https://api.geoapify.com"],
+        scriptSrc: [
+          "'self'",
+          "https://js.stripe.com",
+          "https://api.geoapify.com",
+        ],
         styleSrc: ["'self'"],
-        imgSrc: ["'self'", "data:", "https://storage.googleapis.com", "http://localhost:5001"],
+        imgSrc: [
+          "'self'",
+          "data:",
+          "https://storage.googleapis.com",
+          "http://localhost:5001",
+        ],
         connectSrc: ["'self'", "https://jewelryshop-8q3d.onrender.com"],
         fontSrc: ["'self'", "https://fonts.googleapis.com"],
       },
     },
     crossOriginResourcePolicy: { policy: "cross-origin" }, // Fix for image blocking. In my CSP I'm restricting which domains the cross can happen between
-    referrerPolicy: { policy: "no-referrer" },             // Reducer referrer-lækager
+    referrerPolicy: { policy: "no-referrer" }, // Reducer referrer-lækager
   })
 );
 
-
-
 const PORT = process.env.PORT || 5001;
-const mongoUri = process.env.MONGO_URI || '';
+const mongoUri = process.env.MONGO_URI || "";
 // checks if the current environment is proudction by the NODE_ENV
 const isProduction = process.env.NODE_ENV === "production";
 const frontendURL = isProduction
   ? "https://jewelryshop-two.vercel.app" // Deployed frontend
   : "http://localhost:5173"; // Local frontend
-
-
-
 
 //************************************************************************ */ CORS configuration
 
@@ -112,8 +96,7 @@ app.use(
     credentials: true, // Allowing the cookies and authentication headers
   })
 );
-app.use(express.json()); 
-
+app.use(express.json());
 
 // ********************************** Rate Limiting **********************************
 
@@ -128,49 +111,35 @@ const globalRateLimiter = rateLimit({
 // Apply the rate limiter globally to all routes
 app.use(globalRateLimiter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // **********************************//GENERAL STUFF//****************************************************
 // **********************************//GENERAL STUFF//****************************************************
 // **********************************//GENERAL STUFF//****************************************************
 // **********************************//GENERAL STUFF//****************************************************
 // **********************************//GENERAL STUFF//****************************************************
-
-
 
 // Serve static files from the `public/uploads` directory
-app.use("/uploads", express.static(path.join(__dirname, "../../public/uploads")));
-app.use("/uploads_profilepictures", express.static(path.join(__dirname, "../../public/uploads_profilepictures")));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "../../public/uploads"))
+);
+app.use(
+  "/uploads_profilepictures",
+  express.static(path.join(__dirname, "../../public/uploads_profilepictures"))
+);
 console.log(
   "Serving static files from:",
   path.join(__dirname, "../../public/uploads_profilepictures")
 );
 
-
-
 //************************************************************************ */ MongoDB connection
 mongoose
   .connect(mongoUri)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err: unknown) => console.error('Failed to connect to MongoDB', err));
-  
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err: unknown) => console.error("Failed to connect to MongoDB", err));
 
 // ***************************************HTTPS*********************************************************
 // HTTPS-server opsætning
-// Læser indholdet af filen private.key synkront vha fs (file system) modulet i nodeJS 
+// Læser indholdet af filen private.key synkront vha fs (file system) modulet i nodeJS
 // private key er stien til filen, som indeholder den private nøgle til SSL certifikatet
 // utf8 er filens indhold som skal læses som en streng i UTF-8 kodning, så den privat enøgle returneres som almindelig tekst
 // Resultatet er den private nøgle for SSL certifikatet som er nødvendig for at kryptere/dekryptere den sikre kommunikation mellem serveren og klienten
@@ -188,43 +157,34 @@ mongoose
 
 // const credentials = { key: privateKey, cert: certificate, passphrase: passphrase};
 
-
 // **********************************************************************Adding the routes to the server:
-app.use('/api/products', productsRouter); //  products router
-app.use('/api/auth', authRouter); //  auth router for authentication
+app.use("/api/products", productsRouter); //  products router
+app.use("/api/auth", authRouter); //  auth router for authentication
 app.use("/api/forgotpassword", forgotPasswordRoutes);
-app.use('/api', cartRoutes); // Cart routes
+app.use("/api", cartRoutes); // Cart routes
 app.use("/api/orders", ordersRoutes); // All order routes will now be prefixed with `/api/orders`
 app.use("/api/delivery", deliveryRoutes);
-app.use('/api/payment', paymentRoute);
+app.use("/api/payment", paymentRoute);
 app.use("/api/favourites", favouritesRouter);
 app.use("/api/admin/products", adminProducts);
 app.use("/api/admin/orders", adminOrders);
-app.use('/api/users', userRouter);
-app.use('/api/recycle', recycleRoutes);
-app.use("/api", csrfRoutes);
-
-
-
-
+app.use("/api/users", userRouter);
+app.use("/api/recycle", recycleRoutes);
 
 const staticPath = isProduction
-  ? path.join(__dirname, "uploads") 
+  ? path.join(__dirname, "uploads")
   : path.join(__dirname, "../../public/uploads");
 
 app.use("/uploads", express.static(staticPath));
 
-
-
 // Start the server
 app.listen(PORT, () => {
   console.log(
-    `Server running in ${
-      isProduction ? "production" : "development"
-    } mode on ${isProduction ? "https" : "http"}://localhost:${PORT}`
+    `Server running in ${isProduction ? "production" : "development"} mode on ${
+      isProduction ? "https" : "http"
+    }://localhost:${PORT}`
   );
 });
-
 
 // ***********************PRODUCTION MODE*****************************Create HTTPS server instead of HTTP server
 // https.createServer(credentials, app).listen(PORT, () => {
